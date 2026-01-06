42783ec50b1260b9b314568af365ecd8b6b43d9c
loksill <loksasaloktionov@yandex.ru>
2026-01-06 18:30:53 +0300

    Локализация баланса

--- src/main/java/net/mcreator/ssc/procedures/UplinkBalanceSystemProcedure.java
+++ src/main/java/net/mcreator/ssc/procedures/UplinkBalanceSystemProcedure.java
@@ -4,6 +4,7 @@ import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.component.DataComponents;
 
 import net.mcreator.ssc.init.Ssc14ModItems;
@@ -21,6 +22,7 @@ public class UplinkBalanceSystemProcedure {
 			}
 			(entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).shrink((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getCount());
 		}
-		return "Balance: " + ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDoubleOr("UplinkBalance", 0));
+		return Component.translatable("key.ssc_14.uplinkbalancesystem.balance").getString() + ""
+				+ ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDoubleOr("UplinkBalance", 0));
 	}
 }
